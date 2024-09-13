cc72ffcd6d082c85a14c1ed3c76c221f81393128
udodinho <udodinho200@gmail.com>
2024-09-13 15:22:41 +0100

    fix: import openzeppelin

--- contracts/EventNFT.sol
+++ contracts/EventNFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
